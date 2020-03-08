24b1ef3fc7b4f078fec7d545c7585cf63bfeca65
chenqiao <mrjoechen@qq.com>
2020-03-08 21:37:50 +0800

    泛型

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,9 @@
+import com.sun.org.apache.xerces.internal.xs.datatypes.ObjectList;
 import singleton.EnumSingleton;
 import reference.TestReference;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test {
 
@@ -12,5 +16,51 @@ public class Test {
 
         TestReference testReference = new TestReference();
         testReference.test();
+
+
+        List<Fruit> fruits = new ArrayList<>();
+
+        Fruit fruit = new Banana();
+        Apple apple = new Apple();
+        fruits.add(fruit);
+        fruits.add(apple);
+
+//        List<Fruit> fruitss = new ArrayList<Apple>();
+
+        List<? extends Fruit> fruitList = new ArrayList<>();
+//        fruitList.add(fruit);
+//        fruitList.add(apple);
+        Fruit fruit1 = fruitList.get(0);
+
+
+        List<? super Fruit> fruitList1 = new ArrayList<Object>();
+        fruitList1.add(new Apple());
+        Object object = fruitList1.get(0);
+
+    }
+
+
+
+    static class Fruit{
+
+        void price(){
+
+        }
+    }
+
+    static class Apple extends Fruit{
+
+        @Override
+        public void price() {
+
+        }
+    }
+
+    static class Banana extends Fruit{
+
+        @Override
+        public void price() {
+
+        }
     }
 }
